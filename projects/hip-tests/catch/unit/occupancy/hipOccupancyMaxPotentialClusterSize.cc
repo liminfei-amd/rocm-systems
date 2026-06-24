480578a9792cfc835bdc63e3ac901988242ec1f3
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-06-24 08:45:28 +0100

    SWDEV-515087 - fix merge problem. REQUIRE() testing maximum clusterSize ended up being placed before the actual value was produced with hipOccupancyMaxPotentialClusterSize()

--- projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxPotentialClusterSize.cc
+++ projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxPotentialClusterSize.cc
@@ -46,9 +46,6 @@ TEST_CASE("Unit_hipOccupancyMaxPotentialClusterSize_Positive_RangeValidation") {
 
   if (props.clusterLaunch) {
     INFO("Max potential cluster size is: " << clusterSize);
-    // at the time of this writing a SPI could be drive up to 15 CUs. The number of CUs per SE
-    // varies per silicon, but any AMD silicon should have at least 8
-    REQUIRE((clusterSize >= 8 && clusterSize <= 16));
   } else {
     HIP_SKIP_TEST("cluster launches are not supported on this device");
   }
