3760ed9d98f20d6526b3e779e431602d5a9859f6
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-06-26 10:20:28 -0400

    rocr: Make EXPORT_MEMORY_FLAGS a KfdDriver internal implementation

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
@@ -4187,7 +4187,7 @@ Runtime::VMemorySetAccessPerHandle(void *va, MappedHandle &mappedHandle,
     Agent *ownerAgent = memHandle->agentOwner();
     int dmabuf_fd = -1;
     hsa_status_t status = ownerAgent->driver().ExportMemoryHandle(
-        *ownerAgent, memHandle->driver_handle, ShareType::DMABUF_FD, 0, &dmabuf_fd);
+        *ownerAgent, memHandle->driver_handle, ShareType::DMABUF_FD, &dmabuf_fd);
     if (status != HSA_STATUS_SUCCESS)
       return status;
     memHandle->driver_handle.dmabuf_fd = dmabuf_fd;
@@ -4399,8 +4399,7 @@ hsa_status_t Runtime::VMemoryExportShareableHandle(int* dmabuf_fd,
   auto agentOwner = memoryHandle->region->owner();
 
   return agentOwner->driver().ExportMemoryHandle(*agentOwner, memoryHandle->driver_handle,
-                                                 ShareType::DMABUF_FD,
-                                                 0, dmabuf_fd);
+                                                 ShareType::DMABUF_FD, dmabuf_fd);
 }
 
 hsa_status_t Runtime::VMemoryImportShareableHandle(int dmabuf_fd,
@@ -4430,8 +4429,7 @@ hsa_status_t Runtime::VMemoryExportFabricHandle(hsa_fabric_handle_t* fabric_hand
   auto agentOwner = memoryHandle->region->owner();
 
   return agentOwner->driver().ExportMemoryHandle(*agentOwner, memoryHandle->driver_handle,
-                                                 ShareType::FABRIC_HANDLE,
-                                                 0, fabric_handle);
+                                                 ShareType::FABRIC_HANDLE, fabric_handle);
 }
 
 hsa_status_t Runtime::VMemoryImportFabricHandle(hsa_fabric_handle_t fabric_handle,
